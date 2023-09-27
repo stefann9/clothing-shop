693091d2e84b1cb953a27b17696b9c31b9275120
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-27 19:52:50 +0300

    add signInWithGooglePopup in SignIn comp

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,4 +1,17 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return <div>Hello World</div>;
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <h1>Sign in page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
+    </div>
+  );
 };
-export default SignIn
+
+export default SignIn;
