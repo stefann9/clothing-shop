1a952cdcde1246311097db9a2c35de549d342050
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-27 19:09:01 +0300

    implement auth with firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithRedirect } from "firebase/auth";
 
+// https://firebase.google.com/docs/auth/web/google-signin?authuser=0 --> Authenticate Using Google
 const firebaseConfig = {
   apiKey: "",
   authDomain: "",
@@ -11,3 +13,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const auth = getAuth();
