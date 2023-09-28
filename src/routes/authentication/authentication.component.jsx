cadfdae72acc8cb4d4a6cdde065da28c36cb0fb6
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-28 20:25:08 +0300

    change sign-in comp to authentication

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -6,7 +6,7 @@ import {
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-const SignIn = () => {
+const Authentication = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -23,4 +23,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default Authentication;
