a55f2476241510b065ebcd77897aef96eb26da78
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-29 12:53:40 +0300

    add authentication style

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,11 +1,11 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-const Authentication = () => {
+import "./authentication.style.scss";
 
+const Authentication = () => {
   return (
-    <div>
-      <h1>Sign in page</h1>
+    <div className="authentication-container">
       <SignInForm />
       <SignUpForm />
     </div>
