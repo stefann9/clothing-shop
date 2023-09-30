7d33681e560ffd8a4a8258b04670e3dee1998249
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-30 18:41:44 +0300

    add ProductsContext in Shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,13 @@
-import SHOP_DATA from "../../shop-data.json";
+import { useContext } from "react";
+import { ProductsContext } from "../../contexts/products.context";
+
 const Shop = () => {
+  const { products } = useContext(ProductsContext);
   return (
     <div>
-      {SHOP_DATA.map(({ id, name }) => (
-        <div>
-          <h1 key={id}>{name}</h1>
+      {products.map(({ id, name }) => (
+        <div key={id}>
+          <h1>{name}</h1>
         </div>
       ))}
     </div>
