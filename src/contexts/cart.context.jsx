5b58dc919f402ad0ed188988fc41db4b1d3d0c5b
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-10-01 15:21:11 +0300

    add cartCount in CartContext & use it in CartIcon

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   const productAlreadyInCart = cartItems.find((item) => item.id === productToAdd.id);
@@ -14,15 +14,26 @@ export const CartContext = createContext({
   setIsCartOpen: () => null,
   cartItems: [],
   addItemToCart: () => null,
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
   const addItemsToCart = (productToAdd) => setCartItems(addCartItem(cartItems, productToAdd));
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemsToCart };
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (prevValue, currentValue) => prevValue + currentValue.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemsToCart, cartCount };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
