b6243085907c0b6a4b9b1fa1682ffede177db725
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-10-18 20:10:42 +0300

    use styled component in CartIcon style

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,14 @@
 import { useContext } from "react";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import "./cart-icon.style.scss";
 import { CartContext } from "../../contexts/cart.context";
+import { ShoppingIcon, CartIconContainer, ItemCount } from "./cart-icon.style.jsx";
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   const toggleisCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
-    <div className="cart-icon-container" onClick={toggleisCartOpen}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleisCartOpen}>
+      <ShoppingIcon />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 export default CartIcon;
