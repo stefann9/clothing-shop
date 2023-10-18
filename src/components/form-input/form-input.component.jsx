b60e1a3a0d528165e0213a1668cb148d54394814
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-10-18 20:12:16 +0300

    use styled component in FormInput style

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,14 +1,10 @@
-import "./form-input.style.scss";
+import { FormInputLabel, Group, Input } from "./form-input.style.jsx";
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input className="form-input" {...otherProps} />
-      {label && (
-        <label className={`${otherProps.value.length ? "shrink" : ""} form-input-label`}>
-          {label}
-        </label>
-      )}
-    </div>
+    <Group>
+      <Input {...otherProps} />
+      {label && <FormInputLabel shrink={otherProps.value.length}>{label}</FormInputLabel>}
+    </Group>
   );
 };
 export default FormInput;
