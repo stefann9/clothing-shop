140f59cb1cd520de2efd21cc5b411caa7f8ca108
stefann9 <105439203+stefann9@users.noreply.github.com>
2024-01-24 20:09:02 +0200

    move logger in a new file

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { loggerMiddleware } from "./middleware/logger";
 import { rootReducer } from "./root-reducer";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-
-
-const loggerMiddleware = (store) => (next) => (action) =>{
-    if(!action.type){
-        return next(action);
-    }
-
-    console.log("type: ", action.type);
-    console.log("payload: ", action.payload);
-    console.log("currentState: ", store.getState());
-
-    next(action);
-
-    console.log("next state", store.getState());
-}
 
 const persistConfig = {
     key:"root",
